6aeaded3dceaa72bf5b8f7720f8607ec2dcbb51f
jenifery <jenifers@revature.com>
2023-03-13 10:51:30 -0400

    second

---
+++ BookingApi/Controllers/BookingController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BookingApi.Models;
+
+namespace BookingApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+        private readonly BookContext _context;
+
+        public BookingController(BookContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Booking
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Book>>> GetBookings()
+        {
+          if (_context.Bookings == null)
+          {
+              return NotFound();
+          }
+            return await _context.Bookings.ToListAsync();
+        }
+
+        // GET: api/Booking/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBook(int id)
+        {
+          if (_context.Bookings == null)
+          {
+              return NotFound();
+          }
+            var book = await _context.Bookings.FindAsync(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return book;
+        }
+
+        // PUT: api/Booking/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBook(int id, Book book)
+        {
+            if (id != book.BookingId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(book).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BookExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Booking
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Book>> PostBook(Book book)
+        {
+          if (_context.Bookings == null)
+          {
+              return Problem("Entity set 'BookContext.Bookings'  is null.");
+          }
+            _context.Bookings.Add(book);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBook", new { id = book.BookingId }, book);
+        }
+
+        // DELETE: api/Booking/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBook(int id)
+        {
+            if (_context.Bookings == null)
+            {
+                return NotFound();
+            }
+            var book = await _context.Bookings.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _context.Bookings.Remove(book);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BookExists(int id)
+        {
+            return (_context.Bookings?.Any(e => e.BookingId == id)).GetValueOrDefault();
+        }
+    }
+}

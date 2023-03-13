3e6b1a16fd18347fd36cc511431053e344d44690
jenifery <jenifers@revature.com>
2023-03-13 10:35:24 -0400

    first

---
+++ BookingApi/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookingApi.Models
+{
+    public class Book
+    {
+        [Key]
+        public int BookingId { get; set; }
+        public string BookingName { get; set; }
+        public string MovieName { get; set; }
+        
+    }
+}
---
+++ BookingApi/Models/BookContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace BookingApi.Models
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Bookings { get; set; }
+    }
+}
